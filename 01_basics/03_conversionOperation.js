13688cd6b45a34af9723dd9796b5b925e36871cb
Ishika-prakash <160583269+Ishika-prakash@users.noreply.github.com>
2024-02-21 08:27:58 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+//  let score = "33abc"
+// let score = null
+// let score = undefined
+// let score = true
+let score = "Ishika"
+
+//console.log(typeof score );
+//console.log(typeof (score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber  );
+//console.log( valueInNumber  );
+
+// "33" => 33
+// "33abc" => NaN , type= number
+// true => 1 false =>0
+
+let isloggedIn = 1 
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+// console.log( booleanIsloggedIn);
+
+// 0 => false , 1 =>true 
+// "" => false , "Ishika" => true
+
+let someNumber = 33 
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
