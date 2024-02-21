d5c1ddd7d628401160631c73a554ec842161f2b1
Ishika-prakash <160583269+Ishika-prakash@users.noreply.github.com>
2024-02-21 07:48:50 +0000

    variables and datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+// alert (3+3) // we are using nodejs , not browser
+
+console.log(3+3)
+
+// code readability should be high
+
+console.log("Ishika");
+
+let name = "Ishika"
+let age= 17
+let isloggedIn= false
+
+//data types:
+// number => 2 to the power 53
+// bigint (a big number)
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => unique 
+
+// object 
+
+console.log( typeof undefined); //undefined
+console.log( typeof null); //object
+console.log( typeof "Ishika"); //string
+console.log( typeof age); // number
+console.log( typeof isloggedIn); // boolean
+
