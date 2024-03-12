d6394ca3301ce2b95e49c4c4564872034a471d32
Ishika-prakash <160583269+Ishika-prakash@users.noreply.github.com>
2024-03-12 06:23:40 +0000

    array2

---
+++ 02_basics/02_array.js
@@ -0,0 +1,12 @@
+const marvel_heros = ["thor" , "Ironman", "spiderman"]
+const dc_heros = ["superman" , "flash", "batman"]
+
+//marvel_heros.push(dc_heros)
+
+//console.log(marvel_heros);
+//console.log(marvel_heros[3][1]);
+
+//const all_heros = marvel_heros.concat(dc_heros)
+//console.log(all_heros);
+
+const all_new_heros = [..]
