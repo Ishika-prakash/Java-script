777ca10aa3af2b75bee18de2c8e71808b3f05bc4
Ishika-prakash <160583269+Ishika-prakash@users.noreply.github.com>
2024-03-17 08:16:22 +0000

    one

---
+++ 04_control_flow/one.js
@@ -0,0 +1,30 @@
+//if
+//const isUserloggedIn = true
+//const temperature = 54
+//if (temperature < 50){
+   // console.log("Tempeature is less than 50")
+//}
+//else{
+//console.log("Temperature is greater than 50")
+//}
+// <, >, <=, >=, ==, !=, ===
+
+//const score = 200
+
+//if (score>100){
+  //  const power = "fly"
+  //  console.log(`User power : ${power}`)
+//}
+
+//const balance = 1000 
+//if(balance > 500)console.log("test");
+
+if (balance <500 ) {
+  //  console.log ("less than 50");
+} 
+else if(balance <700 ) {
+   // console.log ("less than 7000");
+} 
+ else if (balance <1000 ) {
+   // console.log ("less than 1000")
+} 
